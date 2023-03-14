c0e397f813abece5d1a886b3bc95ba313584fe14
Dominik Belca <dbelca@content-optimizer.de>
2023-03-14 12:55:49 +0100

    - Minor cleanups

--- src/JotaworksDoiBundle/Controller/DoiController.php
+++ src/JotaworksDoiBundle/Controller/DoiController.php
@@ -23,87 +23,83 @@ use MauticPlugin\JotaworksDoiBundle\QueueName;
 class DoiController extends FormController
 {
 
-    protected function decryptDoiActions($enc) 
+    protected function decryptDoiActions($enc)
     {
-            $leadModel = $this->getModel('lead'); 
-            $encryptionHelper = $this->get('mautic.helper.encryption');
+        $leadModel = $this->getModel('lead');
+        $encryptionHelper = $this->get('mautic.helper.encryption');
 
-            //Get doi parameters
-            if(!$enc)
-            {
-                http_response_code(400);
-                die();
-            }        
+        //Get doi parameters
+        if (!$enc) {
+            http_response_code(400);
+            die();
+        }
 
-            //get base64 string
-            $base64 = Base64Helper::prepare_base64_url_decode($enc);
+        //get base64 string
+        $base64 = Base64Helper::prepare_base64_url_decode($enc);
 
-            //decrypt string
-            $config = $encryptionHelper->decrypt($base64,true);
-            if(!$config ||!is_array($config))
-            {
-                http_response_code(401);
-                die();
-            }
-                       
-            $lead = $leadModel->getEntity($config['lead_id']);
-            if(!$lead)
-            {
-                http_response_code(400);
-                die();
-            }
-            $leadEmail = $lead !== null ? $lead->getEmail() : null;          
-            $config['leadEmail'] = $leadEmail;
+        //decrypt string
+        $config = $encryptionHelper->decrypt($base64, true);
+        if (!$config || !is_array($config)) {
+            http_response_code(401);
+            die();
+        }
 
-            return $config;
+        $lead = $leadModel->getEntity($config['lead_id']);
+        if (!$lead) {
+            http_response_code(400);
+            die();
+        }
+        $leadEmail = $lead !== null ? $lead->getEmail() : null;
+        $config['leadEmail'] = $leadEmail;
+
+        return $config;
     }
 
 
     /**
-     * Doi confirmation action 
-     * 
+     * Doi confirmation action
+     *
      * @param string $enc
      * @return JsonResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function indexAction($enc = false)
-    {               
-            //try to decrypt doi action config
-            $config = $this->decryptDoiActions($enc);   
-                       
-            $queueService = $this->get('mautic.queue.service');
-            if ( $queueService->isQueueEnabled() ) 
-            {   
-                $payload = [
-                    'config' => $config, 
-                    'doiActivationTime' => time(),
-                    'request' => $this->request
-                ];
+    {
+        //try to decrypt doi action config
+        $config = $this->decryptDoiActions($enc);
 
-                $queueService->publishToQueue(QueueName::DOI_SUCCESSFUL, $payload);
+        $queueService = $this->get('mautic.queue.service');
+        if ($queueService->isQueueEnabled()) {
+            $payload = [
+                'config'            => $config,
+                'doiActivationTime' => time(),
+                'request'           => $this->request,
+            ];
 
-            } else {
-                $doiActionHelper = $this->get('jw.doi.actionhelper');
-                $doiActionHelper->applyDoiActions($config);
-            }
-            
-            //redirect to doi success url 
-            return $this->redirect($config['url'], 301);            
+            $queueService->publishToQueue(QueueName::DOI_SUCCESSFUL, $payload);
+
+        } else {
+            $doiActionHelper = $this->get('jw.doi.actionhelper');
+            $doiActionHelper->applyDoiActions($config);
+        }
+
+        //redirect to doi success url
+        return $this->redirect($config['url'], 301);
     }
 
     /**
-     * Click bait action for email scanning bots 
-     * 
+     * Click bait action for email scanning bots
+     *
      * @param string $hash
      */
-    public function nothumanAction($hash = false) 
+    public function nothumanAction($hash = false)
     {
         $notHumanClickHelper = $this->get('jw.doi.nothumanclickhelper');
         $notHumanClickHelper->setClick($hash);
 
         return $this->delegateView([
-            'viewParameters' => [],
-            'contentTemplate' => 'JotaworksDoiBundle:Doi:nothuman.html.php'
-        ]);        
+            'viewParameters'  => [],
+            'contentTemplate' => 'JotaworksDoiBundle:Doi:nothuman.html.php',
+        ]);
     }
 
 }
